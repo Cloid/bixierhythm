df97e3df7a0ee6280d0d406ca848bea8dc471d93
Josue Armando Uriarte Reyes <47550290+JosueUriarte@users.noreply.github.com>
2021-09-23 13:26:15 -0700

    update

--- BixieRhythm/Assets/Scripts/Osu2Unity/GameHandler.cs
+++ BixieRhythm/Assets/Scripts/Osu2Unity/GameHandler.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
+//using UnityEditor;
 using System.IO;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
