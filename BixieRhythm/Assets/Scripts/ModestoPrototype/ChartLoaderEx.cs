982f6de59fa1172b34416a058e3809c740e68fe7
Modesto Amador <modestoamador16@gmail.com>
2021-09-30 19:16:24 -0700

    put chart in

--- BixieRhythm/Assets/Scripts/ModestoPrototype/ChartLoaderEx.cs
+++ BixieRhythm/Assets/Scripts/ModestoPrototype/ChartLoaderEx.cs
@@ -19,7 +19,7 @@ public class ChartLoaderEx : MonoBehaviour
         chartReader = new ChartReader();
         //Chart yoshiChart = chartReader.ReadChartFile("Assets/ChartLoader/ChartLoader/Songs/Yoshi/Yoshi.chart");
         //Chart yoshiChart = chartReader.ReadChartFile(Application.streamingAssetsPath + "/Bixie_Track_1.chart");
-        Chart yoshiChart = chartReader.ReadChartFile(Application.streamingAssetsPath + "/Yoshi.chart");
+        Chart yoshiChart = chartReader.ReadChartFile(Application.streamingAssetsPath + "/Bixie_Track.chart");
 
         Note[] expertGuitarNotes = yoshiChart.GetNotes("ExpertSingle");
         SpawnNotes(expertGuitarNotes);
