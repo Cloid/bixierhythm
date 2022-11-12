8c9374361bec0768a9727b75a87f995f6ddaf6d5
Josue Armando Uriarte Reyes <47550290+JosueUriarte@users.noreply.github.com>
2022-11-11 16:30:24 -0800

    Update GameManager.cs

--- BixieRhythm/Assets/Scripts/ModestoPrototype/GameManager.cs
+++ BixieRhythm/Assets/Scripts/ModestoPrototype/GameManager.cs
@@ -316,7 +316,6 @@ public class GameManager : MonoBehaviour
         GHComboText.text = "Combo: " + multGHTracker;
         multiplierText.text = "Multiplier: x" + currGHMultiplier;
         //ChangePortrait("Bad", 1);
-        
 
         if(currentPlayerHP > 0)
         {
